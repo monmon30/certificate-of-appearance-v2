c92e8f1a860fbbe7ad89e9197c408ce405f5f1aa
monmon30 <69466603+monmon30@users.noreply.github.com>
2023-03-24 07:01:54 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,7 @@
+<script setup>
+import AppearanceForm from "../components/AppearanceForm.vue";
+</script>
+
+<template>
+  <q-page class="flex flex-center" padding> <AppearanceForm /> </q-page>
+</template>
